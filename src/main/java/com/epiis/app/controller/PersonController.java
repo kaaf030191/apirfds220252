f525333b36f9a3d95278dcf6e6bfa0714ba1929d
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-10-09 22:12:51 -0500

    Feat: 09-10-2025 - Envío de datos JSON.
    Envío de datos JSON sobre clase de transporte de respuesta.

--- src/main/java/com/epiis/app/controller/PersonController.java
+++ src/main/java/com/epiis/app/controller/PersonController.java
@@ -4,13 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.epiis.app.controller.reqresp.ResponsePersonInsert;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping(path = "person")
 public class PersonController {
 	@GetMapping(path = "insert")	
-	public ResponseEntity<String> insert() {
-		return new ResponseEntity<>("Hola qué tal?", HttpStatus.OK);
+	public ResponseEntity<ResponsePersonInsert> insert() {
+		ResponsePersonInsert responsePersonInsert = new ResponsePersonInsert();
+		
+		responsePersonInsert.type = "success";
+		responsePersonInsert.message = "Operación realizada correctamente";
+		
+		return new ResponseEntity<>(responsePersonInsert, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/epiis/app/controller/reqresp/ResponsePersonInsert.java
@@ -0,0 +1,11 @@
+package com.epiis.app.controller.reqresp;
+
+public class ResponsePersonInsert {
+	public String type;
+	public String message;
+	
+	public ResponsePersonInsert() {
+		this.type = "error";
+		this.message = "";
+	}
+}
